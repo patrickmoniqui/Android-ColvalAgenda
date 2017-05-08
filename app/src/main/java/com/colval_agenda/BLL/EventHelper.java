7164bbc3b4b15e25925a9b52bb7b060f03a8f69e
jpcomtoisbelair <jp_comtois@hotmail.ca>
2017-05-08 13:54:36 -0400

    added helper and manager

---
+++ app/src/main/java/com/colval_agenda/BLL/EventHelper.java
@@ -0,0 +1,49 @@
+package cegep.comcegepcalender.BLL;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class EventHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "Name";
+    public static final int DB_VERSION = 1;
+    public static final String TB_EVENT = "events";
+    public static final String EVENT_ID = "id";
+    public static final String EVENT_TITLE = "title";
+    public static final String EVENT_DESCRIPTION = "description";
+    public static final String EVENT_LOCATION = "location";
+    public static final String EVENT_COLOR = "color";
+    public static final String EVENT_STARTDATE = "startDate";
+    public static final String EVENT_ENDDATE = "endDate";
+    public static final String EVENT_REMINDER = "reminder";
+
+
+    public EventHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    // SQLite n'a pas de booléen, utiliser un Int. 0 = False , 1 = True
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE" + TB_EVENT + "(" +
+                        EVENT_ID + " integer primary key autoincrement," +
+                        EVENT_TITLE + " text not null," +
+                        EVENT_DESCRIPTION + " text," +
+                        EVENT_LOCATION + " text," +
+                        EVENT_COLOR + " integer," +
+                        EVENT_STARTDATE + " text," +
+                        EVENT_ENDDATE + " text," +
+                        EVENT_REMINDER + " integer" +
+                        ")"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table " + TB_EVENT);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/colval_agenda/BLL/EventManager.java
@@ -0,0 +1,157 @@
+package cegep.comcegepcalender.BLL;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 0756992 on 2017-04-24.
+ */
+public class EventManager {
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    SQLiteOpenHelper helper;
+    SQLiteDatabase database;
+
+    public EventManager(Context context) {
+        helper = new EventHelper(context);
+        database = helper.getWritableDatabase();
+    }
+
+    public void open(){
+        if (!database.isOpen()){
+            database = helper.getWritableDatabase();
+        }
+    }
+
+    public void close(){
+        if (database.isOpen()){
+            database.close();
+        }
+    }
+
+    // SQLite n'a pas de boolean, utiliser un Int. 0 = False , 1 = True
+
+    public void insertEvent(Event event){
+        ContentValues values = new ContentValues();
+        values.put(EventHelper.EVENT_TITLE, event.getTitle());
+        values.put(EventHelper.EVENT_DESCRIPTION, event.getDescription());
+        values.put(EventHelper.EVENT_LOCATION, event.getTitle());
+        values.put(EventHelper.EVENT_COLOR, event.getTitle());
+        values.put(EventHelper.EVENT_STARTDATE, dateFormat.format(event.getStartDate()));
+        values.put(EventHelper.EVENT_ENDDATE, dateFormat.format(event.getFinishDate()));
+        values.put(EventHelper.EVENT_REMINDER, booleanToInt(event.isReminder()));
+        database.insert(EventHelper.TB_EVENT, null, values);
+    }
+
+    public void updateEvent (Event event){
+        ContentValues values = new ContentValues();
+        values.put(EventHelper.EVENT_TITLE, event.getTitle());
+        values.put(EventHelper.EVENT_DESCRIPTION, event.getDescription());
+        values.put(EventHelper.EVENT_LOCATION, event.getTitle());
+        values.put(EventHelper.EVENT_COLOR, event.getTitle());
+        values.put(EventHelper.EVENT_STARTDATE, dateFormat.format(event.getStartDate()));
+        values.put(EventHelper.EVENT_ENDDATE, dateFormat.format(event.getFinishDate()));
+        values.put(EventHelper.EVENT_REMINDER, booleanToInt(event.isReminder()));
+
+        String [] id = new String[1];
+        id[0] = event.getId()+"";
+        database.update(EventHelper.TB_EVENT, values, EventHelper.EVENT_ID + "=?", id);
+    }
+
+    public void deleteEvent (Event event){
+        String[] values = new String[1];
+        values[0] = event.getId()+"";
+        database.delete(EventHelper.TB_EVENT, EventHelper.EVENT_ID + "=?", values);
+    }
+
+    public List<Event> getAllEvents() throws ParseException {
+        Cursor cursor = database.query(EventHelper.TB_EVENT, null,null,null,null,null,null);
+        List<Event> events = new ArrayList<>();
+        if (cursor != null){
+            cursor.moveToFirst();
+            do{
+                Event event = new Event();
+                event.setId(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_ID)));
+                event.setTitle(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_TITLE)));
+                event.setDescription(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_DESCRIPTION)));
+                event.setLocation(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_LOCATION)));
+                event.setColor(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_COLOR)));
+                try {
+                    event.setStartDate(dateFormat.parse(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_STARTDATE))));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    event.setFinishDate(dateFormat.parse(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_ENDDATE))));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                event.setReminder(intToBoolean(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_REMINDER))));
+
+                events.add(event);
+            }while (cursor.moveToNext());
+        }
+        return events;
+    }
+
+    public Event getEventById(int id){
+        String [] values = new String[1];
+        values[0] = id+"";
+        Event event = null;
+        Cursor cursor = database.query(EventHelper.TB_EVENT, null, "id=?", values, null,null,null);
+        if (cursor !=null){
+            cursor.moveToFirst();
+            event = new Event();
+            event.setId(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_ID)));
+            event.setTitle(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_TITLE)));
+            event.setDescription(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_DESCRIPTION)));
+            event.setLocation(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_LOCATION)));
+            event.setColor(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_COLOR)));
+            try {
+                event.setStartDate(dateFormat.parse(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_STARTDATE))));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            try {
+                event.setFinishDate(dateFormat.parse(cursor.getString(cursor.getColumnIndex(EventHelper.EVENT_ENDDATE))));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            event.setReminder(intToBoolean(cursor.getInt(cursor.getColumnIndex(EventHelper.EVENT_REMINDER))));
+        }
+        return event;
+    }
+
+    public int booleanToInt (boolean reminder){
+        int boolToInt = -1;
+        if (reminder == false){
+            boolToInt = 0;
+        } else if (reminder == true){
+            boolToInt = 1;
+        }
+        return boolToInt;
+    }
+
+    public boolean intToBoolean (int reminder){
+        boolean intToBool = false;
+        switch (reminder){
+            case 0:
+                intToBool = false;
+                break;
+            case 1:
+                intToBool = true;
+                break;
+        }
+        return intToBool;
+    }
+
+}
